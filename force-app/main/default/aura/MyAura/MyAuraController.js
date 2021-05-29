53d60db2cad9f857813dfbe9785450a80e8979e9
l-Korrra-l <64279692+l-Korrra-l@users.noreply.github.com>
2021-05-29 06:19:51 +0300

    -

---
+++ force-app/main/default/aura/MyAura/MyAuraController.js
@@ -0,0 +1,11 @@
+({
+    myAction : function(component, event, helper) {
+        
+    },
+    handleClick: function(component, event, helper)
+    {
+        var btnClicked=event.getSource();
+        var btnMessage=btnClicked.get("v.label");
+        component.set("v.message",btnMessage);
+    }
+})
